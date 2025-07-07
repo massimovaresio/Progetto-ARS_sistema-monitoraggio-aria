d415f54843247aba003aff13ba7f3d5400276c68
massimovaresio <varesio.massimo@gmail.com>
2025-07-07 21:56:49 +0200

    Function register user

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Inizializzazione dell'app Flask
 app = Flask(__name__)
@@ -18,6 +19,13 @@ from models import User, AirQuality
 @app.route('/')
 def home():
     return "Benvenuto al sistema di monitoraggio qualità dell'aria!"
+
+def register_user(username, email, password):
+    hashed_password = generate_password_hash(password)
+    new_user = User(username=username, email=email, password_hash=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    print(f"Utente {username} registrato con successo.")
 
 # Avvia il server Flask in modalità debug
 if __name__ == '__main__':
